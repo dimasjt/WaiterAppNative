abcf583b022b41de105830fcf7373604e160fe54
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-10-19 14:12:01 +0700

    use expo

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Expo from "expo"
 import React from "react"
 import { createStore } from "redux"
 import { ApolloProvider as Provider } from "react-apollo"
@@ -8,9 +9,25 @@ import AppReducer from "./reducers"
 import { apolloClient } from "./apollo"
 
 class App extends React.Component {
+  state = { isReady: false }
   store = createStore(AppReducer)
 
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+      MaterialCommunityIcons: require("react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf"),
+    })
+
+    this.setState({ isReady: true })
+  }
+
   render() {
+    if (!this.state.isReady) {
+      return <Expo.AppLoading />
+    }
+
     return (
       <Provider store={this.store} client={apolloClient}>
         <AppNavigator />
@@ -19,4 +36,4 @@ class App extends React.Component {
   }
 }
 
-export default App
+Expo.registerRootComponent(App)
